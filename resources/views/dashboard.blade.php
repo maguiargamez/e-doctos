8f5186e8107f8a0cf6c6f429494997f61259436f
[Manuel] <[manuelaguiargamez@gmail.com]>
2022-05-04 16:00:15 -0500

    04MAY22 - C1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,17 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="h4 font-weight-bold">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('adminlte::page')
+@section('title', 'Dashboard')
 
-    <x-jet-welcome />
-</x-app-layout>
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
